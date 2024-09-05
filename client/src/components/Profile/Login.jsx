f9f010ee8a547072b1f8b927420d4814c9444d60
yakov504 <yakov0546080824@gmail.com>
2024-09-05 09:54:18 +0300

    started db

--- client/src/components/Profile/Login.jsx
+++ client/src/components/Profile/Login.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import SignUp from '../../pages/Signup';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Login() {
   return (
-    <div>
+    <div className='login'>
       <h1>shalom</h1>
     </div>
   )
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import Login from '../components/Profile/Login';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>
-      <h1>i am SignUp</h1>
+    <div className='container'>
+      <div className="head">
+        <h1 className="tital">Hello nice to meet you!</h1>
+      </div>
+      <div className="signUpPlace">
+        <h2>Sign up</h2>
+        <form>
+          <div className="inputs">
+            <div className='input'>
+              <input type="email" name='email' placeholder='Email'/>
+            </div>
+            <div className='input'>
+              <input type="text" name='name' placeholder='Name'/>
+            </div>  
+            <div className='input'>
+              <input type="text" name='phoneNumber' placeholder='Phone number'/>
+            </div> 
+            <div className='input'>
+              <input type="password" name='password' placeholder='Password'/>
+            </div> 
+          </div>
+          <button>Submit!</button>
+          <p className='loginPass'>You how acount? <button style={{color:'rgb(176, 52, 41)'}}>
+            <NavLink to='/Login'> Press here!</NavLink>
+            </button>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
